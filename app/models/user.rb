5ecd0b0fe131845ea5c6e3674de49c51898bbbf1
Hemanth <hemanth.nalamothu@gmail.com>
2016-11-18 05:06:57 +0000

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
-  validates :fname, presence: true, length: {minimum: 3, maximum: 20}
-  validates :lname, presence: true, length: {minimum: 3, maximum: 20}
-  validates :email, presence: true, length: {minimum: 3, maximum: 20}
-  #validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 15}
+  validates :fname, presence: true, 
+            length: {minimum: 3, maximum: 20}
+  validates :lname, presence: true, 
+            length: {minimum: 3, maximum: 20}
+  validates :email, presence: true, length: 
+            {minimum: 3, maximum: 20}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 15}
 end
