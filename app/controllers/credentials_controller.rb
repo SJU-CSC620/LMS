5e5e95b3d73aff75cdc840536717ba694412d79a
Hemanth <hemanth.nalamothu@gmail.com>
2016-11-19 05:45:58 +0000

    complete signup and delete unwanted files

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -1,24 +1,3 @@
 class CredentialsController < ApplicationController
   
-  def signup
-    @details=Credential.new
-  end
-  def create
-    @details = Credential.new(credentials_params)
-    @details.userType="user";
-    # if Credential.find_by(loginId: "")
-    #   flash[:notice] = "Username already exist"
-    #   render 'signup'
-    # end
-    if @details.save
-      flash[:notice] = "Credential was successfully created"
-      redirect_to root_path(@details)
-    else
-      render 'signup'
-    end
-  end
-  private
-  def credentials_params
-      params.require(:credential).permit(:fname, :lname, :email, :password,)
-  end
 end
